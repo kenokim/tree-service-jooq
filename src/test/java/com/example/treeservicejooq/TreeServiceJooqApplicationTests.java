fd8d67c620c0ba7585da9c12e32ea8769180f163
김건호 <keno0403@estsecurity.com>
2023-03-10 22:07:58 +0900

    Initial commit

---
+++ src/test/java/com/example/treeservicejooq/TreeServiceJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treeservicejooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeServiceJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
